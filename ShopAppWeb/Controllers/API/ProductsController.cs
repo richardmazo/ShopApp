cf33d4d0411305119ce2eac888af495a99d7ac50
Richard <richard.mazo.97@hotmail.com>
2019-07-05 22:47:21 -0500

    API Products Controller

---
+++ ShopAppWeb/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace ShopAppWeb.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ShopAppWeb.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
