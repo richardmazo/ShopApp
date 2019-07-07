6610272b4c8f28f4b8f954ee6b94a3f3bb9523e1
Richard <richard.mazo.97@hotmail.com>
2019-07-06 21:31:19 -0500

    Add Views, Views Models and LoginPage

---
+++ ShopApp.UIForms/ShopApp.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopApp.UIForms.Infrastructure
+{
+    using ShopApp.UIForms.ViewModels;
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ ShopApp.UIForms/ShopApp.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ShopApp.UIForms.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+            this.Email = "richard.mazo.15.11@gmail.com";
+            this.Password = "123456";
+        }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter a Email", "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter a password", "Accept");
+                return;
+            }
+            if (!this.Email.Equals("richard.mazo.15.11@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "User or password wrong.", "Accept");
+                return;
+            }
+            await Application.Current.MainPage.DisplayAlert("Ok", "Fuck Yeah!!!", "Accept");
+        }
+    }
+}
